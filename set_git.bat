e96c8062bfdeff86cde396a0e13768a5cd9009c2
alexvirtech <alex@vir-tec.net>
2023-09-02 14:47:25 +0300

    Sat 09-02-23

--- set_git.bat
+++ set_git.bat
@@ -1,6 +1,6 @@
 :: to run use .\set_git.bat
 @echo off
-echo this repository is already initialized, insert :: at the beginning of the rows 2,3,4 in order to reinitialize
+echo this repository is already initialized, insert :: at the beginning of the rows 3,4 in order to reinitialize
 exit /b
 
 echo "# braude-datatable-purejs" >> README.md
